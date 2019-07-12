a76f6171b49856bfdc6da808eb1c0066b2245af5
Akchy <akarshashok12@gmail.com>
2019-07-12 13:43:27 +0530

    bug fixed

--- FDonations/donate/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    title = models.TextField()
-    cover = models.ImageField(upload_to='images/')
-
-    def __str__(self):
-        return self.title
--- blog/vjango/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/davish/ISQIP-Hackathon/blog/vjango/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
